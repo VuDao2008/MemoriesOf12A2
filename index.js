251f26f9434532e60a183360d1c11b6e38083ea7
Nga1'Oz <144659373+VuDao2008@users.noreply.github.com>
2025-12-15 13:55:24 +0700

    Refactor myFunction to use names instead of links
    Signed-off-by: Nga1'Oz <144659373+VuDao2008@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,108 +1,108 @@
 function myFunction() {
          
-         document.getElementById("Text").innerHTML = "";
-         document.getElementById("Link1").innerHTML = "";
-         document.getElementById("Link2").innerHTML = "";
-         document.getElementById("Link3").innerHTML = "";
-         document.getElementById("Link4").innerHTML = "";
-         document.getElementById("Link5").innerHTML = "";
-         document.getElementById("Link6").innerHTML = "";
-         document.getElementById("Link7").innerHTML = "";
-         document.getElementById("Link8").innerHTML = "";
-         document.getElementById("Link9").innerHTML = "";
-         document.getElementById("Link10").innerHTML = "";   
-         document.getElementById("Link11").innerHTML = "";   
+      document.getElementById("Text").innerHTML = "";
+      document.getElementById("Link1").innerHTML = "";
+      document.getElementById("Link2").innerHTML = "";
+      document.getElementById("Link3").innerHTML = "";
+      document.getElementById("Link4").innerHTML = "";
+      document.getElementById("Link5").innerHTML = "";
+      document.getElementById("Link6").innerHTML = "";
+      document.getElementById("Link7").innerHTML = "";
+      document.getElementById("Link8").innerHTML = "";  
+      document.getElementById("Link9").innerHTML = "";
+      document.getElementById("Link11").innerHTML = "";   
+      document.getElementById("Link12").innerHTML = "";
 
          var text;
          var person;
          var LinkCheck = document.getElementById("name").value;
 
          switch(LinkCheck) {
-            case "https://www.facebook.com/long.hoang.7168":
+            case "Long Hoàng":
                   person = "Người được chọn: Hoàng Long";
                   text = ">>Nhấp vào đây<<";
                   document.getElementById("Text").innerHTML = person; 
                   document.getElementById("Link1").innerHTML = text;
             break;
 
-            case "https://www.facebook.com/le.hong.905038":
+            case "Lê Hồng":
                   person = "Người được chọn: Hồng Ánh";
                   text = ">>Nhấp vào đây<<";
                   document.getElementById("Text").innerHTML = person; 
                   document.getElementById("Link2").innerHTML = text;
             break;
 
-            case "https://www.facebook.com/profile.php?id=61580601516395":
+            case "Vannie Vũ":
                   person = "Người được chọn: Vân Anh";
                   text = ">>Nhấp vào đây<<";
                   document.getElementById("Text").innerHTML = person; 
                   document.getElementById("Link3").innerHTML = text;
             break;
 
-            case "https://www.facebook.com/Omegaff0048":
+            case "Phạm Nguyễn Trí Dũng":
                   person = "Người được chọn: Dũng";
                   text = ">>Nhấp vào đây<<";
                   document.getElementById("Text").innerHTML = person; 
                   document.getElementById("Link4").innerHTML = text;
             break;
 
-            case "https://www.facebook.com/hienvuminh528":
+            case "Hien Vu":
                   person = "Người được chọn: Hiển";
                   text = ">>Nhấp vào đây<<";
                   document.getElementById("Text").innerHTML = person; 
                   document.getElementById("Link5").innerHTML = text;
             break;
 
-            case "https://www.facebook.com/thuy.linh.nguyen.855272":
+            case "Thuỳ Linh Nguyễn":
                   person = "Người được chọn: cô Linh chú Dũng ";
                   text = ">>Nhấp vào đây<<";
                   document.getElementById("Text").innerHTML = person; 
                   document.getElementById("Link6").innerHTML = text;
             break;
 
-            case "https://www.facebook.com/bui.nga.186941":
+            case "Bùi Nga":
                   person = "Người được chọn: Nga & Khôi";
                   text = ">>Nhấp vào đây<<";
                   document.getElementById("Text").innerHTML = person; 
                   document.getElementById("Link7").innerHTML = text;
             break;
 
-            case "https://www.facebook.com/khoi.bui.604070":
+            case "Khôi Bùi":
                   person = "Người được chọn: Nga & Khôi";
                   text = ">>Nhấp vào đây<<";
                   document.getElementById("Text").innerHTML = person; 
                   document.getElementById("Link7").innerHTML = text;
             break;
 
-            case "https://www.facebook.com/duc.dao.420513":
+            case "Duc Dao":
                   person = "Người được chọn: anh Đức & chị Hoàng Anh";
                   text = ">>Nhấp vào đây<<";
                   document.getElementById("Text").innerHTML = person; 
                   document.getElementById("Link8").innerHTML = text;
             break;
 
-            case "https://www.facebook.com/thanhhuyen.gdc":
+            case "Thanhhuyen Nguyen":
                   person = "Người được chọn: bố Bình & mẹ Huyền";
                   text = ">>Nhấp vào đây<<";
                   document.getElementById("Text").innerHTML = person; 
                   document.getElementById("Link9").innerHTML = text;
             break;
 
-            case "https://www.facebook.com/Binh.DaoQ":
+            case "Binh DQ":
                   person = "Người được chọn: bố Bình & mẹ Huyền";
                   text = ">>Nhấp vào đây<<";
                   document.getElementById("Text").innerHTML = person; 
                   document.getElementById("Link9").innerHTML = text;
             break;
             
-            case"https://www.facebook.com/YuukiTT1710":
+            case"Trang Nguyễn":
                   person = "Người được chọn: Trang";
                   text = ">>Nhấp vào đây<<";
                   document.getElementById("Text").innerHTML = person; 
                   document.getElementById("Link11").innerHTML = text;
             break;
 
-            case"https://www.facebook.com/vu.dao.1006":
+            case"Vũ Đào":
                   person = "Người được chọn: Vũ Đào";
                   text = ">>Nhấp vào đây<<";
                   document.getElementById("Text").innerHTML = person; 
